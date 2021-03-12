e790cfa97f7257dcb659b30976876af920c1e629
Simon R Jones <simon@studio24.net>
2021-03-12 20:25:56 +0000

    Slack notification

--- all.php
+++ all.php
@@ -1,10 +1,10 @@
 <?php
 
 // Require all Studio 24 Deployer tasks
-require __DIR__ . '/src/sync-down.php';
-require __DIR__ . '/src/build-summary.php';
-require __DIR__ . '/src/show-summary.php';
-require __DIR__ . '/src/wordpress-install.php';
-require __DIR__ . '/src/check-branch.php';
-require __DIR__ . '/src/confirm-continue.php';
-require __DIR__ . '/src/display-disk-space.php';
+require_once __DIR__ . '/src/sync-down.php';
+require_once __DIR__ . '/src/build-summary.php';
+require_once __DIR__ . '/src/show-summary.php';
+require_once __DIR__ . '/src/wordpress-install.php';
+require_once __DIR__ . '/src/check-branch.php';
+require_once __DIR__ . '/src/confirm-continue.php';
+require_once __DIR__ . '/src/display-disk-space.php';
