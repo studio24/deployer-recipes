eb41d7f36addd525ca81c7d00fdf4d8375af5ee5
Simon R Jones <simon@studio24.net>
2021-03-12 17:52:08 +0000

    Add repo link, compact show deploy summary

--- src/show-summary.php
+++ src/show-summary.php
@@ -35,18 +35,38 @@ task('s24:show-summary', function () {
         }
     }
 
+    // Get repo link
+    if (preg_match('/^(.+)@(.+):(.+)\.git$/i', get('repository'), $m)) {
+        $gitHost = $m[2];
+        $gitRepo = $m[3];
+
+        switch ($gitHost) {
+            case 'github.com':
+                $buildData['git_commit_url'] = 'https://github.com/' . $gitRepo . '/commits/' .  $buildData['git_commit'];
+                break;
+            case 'bitbucket.org':
+                $buildData['git_commit_url'] = 'https://bitbucket.org/' . $gitRepo . '/commit/' .  $buildData['git_commit'];
+                break;
+            case 'gitlab.org':
+                $buildData['git_commit_url'] = 'https://gitlab.com/' . $gitRepo . '/-/commit/' . $buildData['git_commit'];
+                break;
+        }
+    }
+
     // Output last build info
     $date = new \DateTimeImmutable($buildData['deploy_datetime']);
     $summary = sprintf(
-        'Currently deployed branch on the <fg=blue;options=bold>%s</> environment is <fg=red;options=bold>%s</>, deployed on <fg=black;options=bold>%s</> by <fg=green;options=bold>%s</>.',
-        $buildData['environment'],
-        $buildData['git_branch'],
-        $date->format('l, F d \a\t h:iA'),
+        '<options=bold>Last build:</> <info>%s</> deployed <info>%s</> branch to <info>%s</> environment on <info>%s</>',
         $buildData['deployed_by'],
+        $buildData['git_branch'],
+        $buildData['environment'],
+        $date->format('r')
     );
 
-    writeLn(' ');
-    writeLn('<options=bold>Last build summary:</>');
+    writeLn('');
     writeLn($summary);
-    writeLn(' ');
+    if (isset($buildData['git_commit_url'])) {
+        writeLn('<options=bold>View git commit:</> ' . $buildData['git_commit_url']);
+    }
+    writeLn('');
 });
