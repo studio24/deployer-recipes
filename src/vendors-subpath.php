e035e4d9f7c2eb96467bb914ae97ea1e2595f2d8
Simon R Jones <simon@studio24.net>
2021-03-13 09:49:08 +0000

    Remove unused local variable

--- src/vendors-subpath.php
+++ src/vendors-subpath.php
@@ -5,9 +5,6 @@ desc('Run composer install in a sub-path');
 task('s24:vendors-subpath', function () {
     $previousOptions = get('composer_options');
     $paths = get('composer_paths');
-    $composer_paths = [
-        'web/wp-content/plugins/s24-wp-image-optimiser'
-    ];
 
     foreach ($paths as $path) {
         set('composer_options', $previousOptions . ' --working-dir=' . $path);
