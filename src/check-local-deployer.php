1ddc1014ad733f10f506ec4d646e7b5d5a397cd7
Alan Isaacson <alan@studio24.net>
2021-03-31 10:09:31 +0100

    Update recipe for clarity

--- src/check-local-deployer.php
+++ src/check-local-deployer.php
@@ -8,6 +8,6 @@ task('s24:check-local-deployer', function () {
     $scriptPath = get_included_files()[0];
 
     if (strpos($scriptPath, __DIR__.'/vendor/deployer/deployer/bin/dep') !== true) {
-        throw new \RuntimeException("Pleaae run using local deployer with ./vendor/bin/dep");
+        throw new \RuntimeException("Pleaae run using local Deployer with ./vendor/bin/dep");
     }
 });
