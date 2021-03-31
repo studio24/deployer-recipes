ce16195ded9ee3ee85ca07a3551e9377b09013c7
Alan Isaacson <alan@studio24.net>
2021-03-31 09:46:59 +0100

    Remove redundant check in if statement

--- src/check-local-deployer.php
+++ src/check-local-deployer.php
@@ -7,9 +7,7 @@ task('s24:check-local-deployer', function () {
 
     $scriptPath = get_included_files()[0];
 
-    if (strpos($scriptPath, __DIR__.'/vendor/deployer/deployer/bin/dep') !== false) {
-        writeln(' ');
-    } else {
+    if (strpos($scriptPath, __DIR__.'/vendor/deployer/deployer/bin/dep') !== true) {
         throw new \RuntimeException("Pleaae run using local deployer with ./vendor/bin/dep");
     }
 });
