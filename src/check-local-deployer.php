58fbd17f22227e98746de034513ef379d4fdf3a7
Alan Isaacson <alan@studio24.net>
2021-04-08 17:43:15 +0100

    Update deployer check for easier to read error

--- src/check-local-deployer.php
+++ src/check-local-deployer.php
@@ -8,6 +8,11 @@ task('s24:check-local-deployer', function () {
     $scriptPath = get_included_files()[0];
 
     if (strpos($scriptPath, '/vendor/deployer/deployer/bin/dep') === false) {
-        throw new \RuntimeException("Pleaae run using local Deployer with ./vendor/bin/dep");
+        writeln("");
+        writeln("<comment>Please run using local Deployer with ./vendor/bin/dep</>");
+        writeln("");
+    }
+    if (!askConfirmation('Continue with deployment?')) {
+        die('Ok, deployment cancelled.');
     }
 });
