4653b60e93e0f7eb141bb0813ddb6bce93941e11
Alan Isaacson <38862429+AlanJIsaacson@users.noreply.github.com>
2023-02-10 17:18:46 +0000

    Updated recipes for deployer 7

--- tasks/confirm-continue.php
+++ tasks/confirm-continue.php
@@ -3,8 +3,10 @@
 namespace Deployer;
 
 desc('Ask confirmation from user before continuing with deployment');
-    task('s24:confirm-continue', function () {
-        if (!askConfirmation('Continue with deployment?')) {
-            die('Ok, deployment cancelled.');
-        }
-    });
+task('s24:confirm-continue', function () {
+    if (!askConfirmation('Continue with deployment?')) {
+
+        throw new \Exception('Deployment aborted.');
+    }
+});
+
--- tasks/display-disk-space.php
+++ tasks/display-disk-space.php
@@ -6,7 +6,11 @@ use Deployer\Task\Context;
 
 desc('Display server disk usage prior to deployment');
 task('s24:display-disk-space', function () {
-    $target = Context::get()->getHost();
-    output()->write(runLocally("ssh $target df -kh --exclude-type=tmpfs --exclude-type=devtmpfs"));
+    $target = Context::get()->getHost()->getLabels();
+
+    output()->write(
+        runLocally("ssh ". $target['stage']. " df -kh --exclude-type=tmpfs --exclude-type=devtmpfs") . "\n"
+    );
     writeln(' ');
+
 });
