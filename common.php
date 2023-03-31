419ff9e3ced5817c64832ab6429f60f7394805d4
Simon R Jones <simon@studio24.net>
2023-03-31 12:59:56 +0100

    Delete Slack task

--- common.php
+++ common.php
@@ -10,7 +10,6 @@ require_once __DIR__ . '/tasks/check-branch.php';
 require_once __DIR__ . '/tasks/confirm-continue.php';
 require_once __DIR__ . '/tasks/display-disk-space.php';
 require_once __DIR__ . '/tasks/vendors-subpath.php';
-require_once __DIR__ . '/tasks/notify-slack.php';
 require_once __DIR__ . '/tasks/check-local-deployer.php';
 
 // Set up defaults
---
+++ docs/upgrading.md
@@ -0,0 +1,9 @@
+# Updrading
+
+# Upgrading to Deployer 7 (v2.0 of deployer-recipes)
+
+TODO
+
+## Removed
+
+* Removed the custom task s24:notify-slack since we can use the standard Slack contrib task in Deployer.
